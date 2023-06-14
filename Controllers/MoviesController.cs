8697c21f3b18200d14d59bdaf6abfe398547bb5d
Sithum <Sithum@DESKTOP-9PSV87A>
2023-06-14 15:36:26 +0530

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Index
+        public ActionResult Index()
+        {
+            //var movie = new Movie() { Name="!Shrek"};
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movie = movie;
+            //var viewResult = new ViewResult();
+            //viewResult.ViewData.Model;
+
+            //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+            //public ActionResult ByReleaseDate(int year, int month)
+            //{
+            //    return Content(year+"/"+month);
+            //}
+
+            //var customers = new List<Customer>
+            //{ new Customer{ Name = "Customer 1"}, new Customer{ Name ="Customer 2"} };
+
+            //var viewModel = new RandomMovieViewModel
+            //{ 
+            //Movie = movie,
+            //Customers = customers
+            //};
+            //return View(viewModel);
+
+           
+            return View(GetMovies());
+        }
+
+        public List<Movie>GetMovies() 
+        {
+            return new List<Movie> { new Movie { Id = 0, Name = "Shrek" }, new Movie { Id = 1, Name = "Titanic" } };
+           
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            return View(GetMovies().SingleOrDefault(c=>c.Id==id));
+        }
+        
+    }
+}
